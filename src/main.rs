8b88bee80b6f065601943d38f9f480e3164120c2
Finn Jonas Peper <j.jonas.peper@gmail.com>
2026-06-24 11:14:25 +0200

    simulation as plugin

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,15 @@
 mod creature;
+mod simulation;
 mod visualization;
 
 use bevy::prelude::*;
-use creature::{Animal, EnergyPosition, Movable, Plant};
+use simulation::SimulationPlugin;
 use visualization::VisualizationPlugin;
-
-const WORLD_HALF_WIDTH: f32 = 400.0;
-const WORLD_HALF_HEIGHT: f32 = 250.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(SimulationPlugin)
         .add_plugins(VisualizationPlugin)
-        .add_systems(Startup, setup_world)
-        .add_systems(Update, move_creatures)
         .run();
-}
-
-fn move_creatures(mut query: Query<(&mut Animal, &mut Transform)>, time: Res<Time>) {
-    for (mut animal, mut transform) in &mut query {
-        transform.translation += animal.velocity().extend(0.0) * time.delta_secs();
-        ensure_torodial_world(&mut transform.translation);
-        animal.set_position(transform.translation.xy());
-    }
-}
-
-fn ensure_torodial_world(translation: &mut Vec3) {
-    if translation.x < -WORLD_HALF_WIDTH {
-        translation.x = WORLD_HALF_WIDTH;
-    } else if translation.x > WORLD_HALF_WIDTH {
-        translation.x = -WORLD_HALF_WIDTH;
-    }
-
-    if translation.y < -WORLD_HALF_HEIGHT {
-        translation.y = WORLD_HALF_HEIGHT;
-    } else if translation.y > WORLD_HALF_HEIGHT {
-        translation.y = -WORLD_HALF_HEIGHT;
-    }
-}
-
-fn setup_world(mut commands: Commands) {
-    let animal = Animal {
-        position: Vec2::new(0.0, 0.0),
-        velocity: Vec2::new(100.0, 1.0),
-        energy: 100.0,
-        radius: 20.0,
-        color: Color::srgb(0.8, 0.2, 0.4),
-    };
-
-    let plant = Plant {
-        position: Vec2::new(-200.0, 100.0),
-        energy: 60.0,
-        radius: 14.0,
-        color: Color::srgb(0.3, 0.6, 0.2),
-    };
-
-    commands.spawn(animal);
-    commands.spawn(plant);
 }
